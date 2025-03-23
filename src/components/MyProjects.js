7954f830472ee0a690d787b997f356f8e94712ed
BivekKarki <66402371+BivekKarki@users.noreply.github.com>
2025-03-23 20:46:35 +0545

    myproject section init

---
+++ src/components/MyProjects.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MyProjects = () => {
+  return (
+    <>
+        <section className='bg-[#030712] py-16'>
+        <div className='container mx-auto'>
+            <div className='text-center mb-20'>
+                <h1 className='text-5xl font-bold text-white mb-6'>Recent Work</h1>
+                <p className='text-3xl text-[#A7A7A7]'> Things I built so far</p>
+            </div>
+            <div className='flex flex-wrap justify-center gap-8 md:gap-20 md:w-2/3 mx-auto'>
+               
+            </div>
+        </div>
+       
+      </section> 
+    </>
+  )
+}
+
+export default MyProjects
